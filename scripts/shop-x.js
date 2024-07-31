67477505849d9fc2ad5a366c4184bbf2356cb574
manojAL <manoj35ramesh@gmail.com>
2024-07-31 21:00:36 +0530

    commut

--- scripts/shop-x.js
+++ scripts/shop-x.js
@@ -1,38 +1,27 @@
-const products=[{product_img:"images/products/athletic-cotton-socks-6-pairs.jpg",
-    product_name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
-    product_rating:{star:4.5,count:87},
-    product_price_paisa:1090},
-{product_img:"images/products/intermediate-composite-basketball.jpg",
-    product_name:"Intermediate Size Basketball",
-    product_rating:{star:4,count:127},
-    product_price_paisa:2095},
-{product_img:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    product_name:"Adults Plain Cotton T-Shirt - 2 Pack",
-    product_rating:{star:4.5,count:56},
-    product_price_paisa:799}]
+
 let productsHTML='';
 products.forEach((product)=>{
-  productsHTML+=html;
-const html=`<div class="product-container">
+  productsHTML+=`
+       <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/${products.product_name}.jpg">
+              src=${product.image}>
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${products.product_name}
+            ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/${(products.product_rating.star)*10}.png">
+              src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-             ${products.product_rating.count}
+             ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            ${(products.product_price_paisa/100).toFixed(2)}
+            ${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -57,10 +46,16 @@ const html=`<div class="product-container">
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" 
+          data-product-name="${product.name}">
             Add to Cart
           </button>
-        </div>`
-})    
-document.querySelector("js-products-grid").
-innerHTML=productsHTML
+        </div>`;
+        
+}) ;
+document.querySelector(".js-products-grid").innerHTML=productsHTML;
+document.querySelectorAll("js-add-to-cart").forEach((button)=>{
+button.addEventListener("click",()=>{
+  console.log(button.data);
+})
+})
